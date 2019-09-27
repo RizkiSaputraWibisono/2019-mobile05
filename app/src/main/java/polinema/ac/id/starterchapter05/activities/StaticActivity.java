5a6254f69636531bc7b2513b3d9046533dacd117
RizkiSaputraWibisono <rizkisaputra329@gmail.com>
2019-09-27 15:44:28 +0700

    Menambahkan package activities, fragment, menambahkan warna red dan blue fragment serta Embed warna red dan blue

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/StaticActivity.java
@@ -0,0 +1,18 @@
+package polinema.ac.id.starterchapter05.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import polinema.ac.id.starterchapter05.R;
+
+public class StaticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_static);
+
+    }
+}
